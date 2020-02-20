338abdc1c3d7281b1b7262186e671d7cc24218bd
peter.dai <peter.dai@dadaabc.net>
2020-02-20 10:06:19 +0800

    add 数据库及表单

--- forms.py
+++ forms.py
@@ -7,3 +7,13 @@
 @time: 2020/1/14 08:53
 @desc:
 """
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class HelloForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
+    body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
+    sumbmit = SubmitField()
+
