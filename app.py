6b11a8e43180815dcdcf726e3f7be73e8f2ba9c5
peter.dai <peter.dai@dadaabc.net>
2020-03-25 08:44:36 +0800

    init project

--- app.py
+++ app.py
@@ -10,7 +10,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bootstrap import Bootstrap
 # 初始化 flask 程序实例
 
 app = Flask(__name__)
@@ -19,6 +19,7 @@ app = Flask(__name__)
 dev_db = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
 # 初始化一个数据库实例
 db = SQLAlchemy(app)
+boostrap = Bootstrap(app)
 
 
 if __name__ == '__main__':
